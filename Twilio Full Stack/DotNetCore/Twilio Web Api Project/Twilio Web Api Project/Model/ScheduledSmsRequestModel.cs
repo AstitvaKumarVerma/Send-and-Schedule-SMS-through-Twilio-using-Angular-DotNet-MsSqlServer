29287e26fdda864cb85ccd82774185a8dfd56a32
Astitva Kumar Verma <101284322+AstitvaKumarVerma@users.noreply.github.com>
2023-09-07 11:46:36 +0530

    Update ScheduledSmsRequestModel.cs

--- Twilio Full Stack/DotNetCore/Twilio Web Api Project/Twilio Web Api Project/Model/ScheduledSmsRequestModel.cs
+++ Twilio Full Stack/DotNetCore/Twilio Web Api Project/Twilio Web Api Project/Model/ScheduledSmsRequestModel.cs
@@ -4,7 +4,6 @@
     {
         public string ToPhoneNumber { get; set; }
         public string Message { get; set; }
-        //public DateTime ScheduledTime { get; set; } // The date and time to send the message
         public string ScheduledTime { get; set; } // Use string for date and time
 
     }
